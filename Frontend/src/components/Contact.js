650e437441c67eab4941b3290ee59a468fcf1b37
Chetna09 <85148693+chetna09-git@users.noreply.github.com>
2021-06-29 15:50:04 +0530

    Add files via upload

---
+++ Frontend/src/components/Contact.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import { CenterFocusStrong } from '@material-ui/icons';
+import Container from '@material-ui/core/Container';
+import Image from '../src/img/doctor.jpg'
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+   
+  },
+  image: {
+    background: 'linear-gradient(to right bottom,#ff99bb, #ffffff)',
+    // backgroundImage: `url(${Image})`
+    // backgroundRepeat: 'no-repeat',
+    // backgroundColor:
+    //   theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    // backgroundSize: 'cover',
+    // backgroundPosition: 'center',
+  },
+paper: {
+  marginTop: theme.spacing(8),
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+},
+  
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.dark,
+  },
+  form: {
+    width: '80%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignInSide() {
+  const classes = useStyles();
+
+  return (
+
+    <Container component="main" maxWidth="xs" className={classes.image}>
+    <CssBaseline />
+    <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <MailOutlineIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Contact Us
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="mobno"
+              label="Mobile Number"
+              type="text"
+              id="mobno"
+              autoComplete="mobno"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="msg"
+              label="Message"
+              name="msg"
+              autoComplete="msg"
+              autoFocus
+            />
+    
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="secondary"
+              className={classes.submit}
+            >
+              Submit
+            </Button>
+           
+            <Box mt={5}>
+                
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+     </Container>
+  );
+}
